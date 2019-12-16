3e5b922fb28f3ba129c257cdc690d94a92c0e67b
sonyasalionovych <33807874+sonyasalionovych@users.noreply.github.com>
2019-12-16 18:31:17 +0100

    Add files via upload

---
+++ servers.js
@@ -0,0 +1,7 @@
+const http = require('http'); // подключаем http модуль!
+            http.createServer((req, res) => {
+            	if(req.url === '/data'){
+            		res.end('SUCCESS')
+            	}
+                res.end(JSON.stringify({ name: 'SERGEi', lastName: 'SHAKHOV' }));
+            }).listen(3000, '127.0.0.1', () => console.log('Server is listening on port:' + 3000) );
